a0ac9bb078fe7626b984f60b7bf3dea55525d6ee
marouan <marmakh12345@gmail.com>
2026-05-19 23:47:51 +0100

    implement CRUD REST API with PostgreSQL using chi and sqlx

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var DB *sqlx.DB
+
+func Connect() error {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("SSL_MODE"),
+	)
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return err
+	}
+	DB = db
+	fmt.Println("Connected to db")
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module todo
 
 go 1.26.2
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require github.com/go-chi/chi/v5 v5.2.5
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
